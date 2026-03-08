470e58fc65c5a5ebfe3341e2ce994e1e853ac802
Leechungwoon <dlcjddns22@gmail.com>
2026-03-09 02:22:13 +0900

    Initial commit

---
+++ src/main/java/com/example/cru/CruApplication.java
@@ -0,0 +1,13 @@
+package com.example.cru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruApplication.class, args);
+    }
+
+}
