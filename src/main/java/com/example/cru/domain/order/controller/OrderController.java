c197d37aa7e24c88c73527587b85ef232663898e
Leechungwoon <dlcjddns22@gmail.com>
2026-03-12 17:37:00 +0900

    feat: 주문 생성 기능 추가 POST 기능 추가 -> 인증/인가 적용 전이기에 @PathVariable 사용하여 userId 받아서 사용

--- src/main/java/com/example/cru/domain/order/controller/OrderController.java
+++ src/main/java/com/example/cru/domain/order/controller/OrderController.java
@@ -1,10 +1,9 @@
 package com.example.cru.domain.order.controller;
 
 import com.example.cru.common.model.CommonResponse;
-import com.example.cru.domain.order.model.request.CreatedOrderRequest;
+import com.example.cru.domain.order.model.request.CreateOrderRequest;
 import com.example.cru.domain.order.model.response.CreateOrderResponse;
 import com.example.cru.domain.order.service.OrderService;
-import com.example.cru.domain.order_item.model.request.OrderItemRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,4 +13,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+
+    // 주문 정보 생성
+    @PostMapping("order/users/{userId}")
+    public ResponseEntity<CommonResponse> createOrder(
+            @PathVariable Long userId,
+            @RequestBody CreateOrderRequest request
+    ) {
+
+        // 비지니스 로직
+        CreateOrderResponse response = orderService.orderCreateService(userId, request);
+
+        // Dto 반환
+        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success("주문에 성공하셨습니다.", response));
+    }
 }
