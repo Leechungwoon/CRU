c541b0696f36623ffe6a3bfa4ffd064475204dd0
Leechungwoon <dlcjddns22@gmail.com>
2026-03-18 19:04:52 +0900

    feat:조회(상세, 목록) 구현, 주문 취소 구현
    - JWT는 추후 적용 예정
    - N+1 발생 가능성 인지 이후 수정 예정
    - Exception은 커스텀 예정
    
    1. 주문 상세 조회
    - 주문 정보 조회, 목록에서 조회
    
    2. 주문 목록 조회
    - 페이징 적용하여 생성일 기준으로 내림차순 정렬
    
    3. 주문 취소 로직
    - PENDING 상태에서만 취소 가능
    - 취소 시 CANCELED로 변경됨

--- src/main/java/com/example/cru/domain/order/controller/OrderController.java
+++ src/main/java/com/example/cru/domain/order/controller/OrderController.java
@@ -1,11 +1,17 @@
 package com.example.cru.domain.order.controller;
 
 import com.example.cru.common.model.CommonResponse;
+import com.example.cru.common.model.PageResponse;
 import com.example.cru.domain.order.model.request.CreateOrderRequest;
 import com.example.cru.domain.order.model.response.CreateOrderResponse;
 import com.example.cru.domain.order.model.response.GetDetailOrderResponse;
+import com.example.cru.domain.order.model.response.GetOrderListResponse;
 import com.example.cru.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +24,7 @@ public class OrderController {
 
     // 주문 정보 생성
     @PostMapping("order/users/{userId}")
-    public ResponseEntity<CommonResponse> createOrder(
-            @PathVariable Long userId,
-            @RequestBody CreateOrderRequest request
-    ) {
+    public ResponseEntity<CommonResponse> createOrder(@PathVariable Long userId, @RequestBody CreateOrderRequest request) {
 
         // 비지니스 로직
         CreateOrderResponse response = orderService.orderCreateService(userId, request);
@@ -31,14 +34,33 @@ public class OrderController {
     }
 
     // 주문 정보 조회
-    @GetMapping("order/get/{userId}")
-    public ResponseEntity<CommonResponse> getDetailOrder(
-            @PathVariable Long userId
-    ) {
+    @GetMapping("get/order/{userId}")
+    public ResponseEntity<CommonResponse> getDetailOrder(@PathVariable Long orderId, @PathVariable Long userId) {
         // 비지니스 로직
-        GetDetailOrderResponse response = orderService.getDetailOrder(userId);
+        GetDetailOrderResponse response = orderService.getDetailOrder(orderId, userId);
 
         //Dto 반환
-        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success("주무 조회에 성공하셨습니다", response));
+        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success("주문 조회에 성공하셨습니다", response));
+    }
+
+    //주문 목록 조회 -> 페이징 적용
+    @GetMapping("users/orderList")
+    public ResponseEntity<PageResponse> getOrderList(@PathVariable Long userId, @PageableDefault(page = 0, size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        //페이징 적용된 비지니스 로직
+        Page<GetOrderListResponse> responses = orderService.getAllOrder(userId, pageable);
+
+        //Dto 반환
+        return ResponseEntity.ok().body(PageResponse.success("주문 목록 조회에 성공하셨습니다", responses));
+    }
+
+    @DeleteMapping("/orders/{orderId}/users/{userId}/cancel")
+    public ResponseEntity<Void> cancelOrder(@PathVariable Long orderId, @PathVariable Long userId) {
+
+        //비지니스 로직
+        orderService.cancelOrder(orderId, userId);
+
+        //삭제 시 메세지 반환
+        return ResponseEntity.noContent().build();
     }
 }
