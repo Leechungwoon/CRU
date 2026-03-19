1d1f79c244c8836ca8f19e8db268cd48d2d74d95
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 16:44:27 +0900

    feat: 결제 여부 확인에 사용될 정적 쿼리 추가

--- src/main/java/com/example/cru/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/example/cru/domain/payment/repository/PaymentRepository.java
@@ -2,6 +2,8 @@ package com.example.cru.domain.payment.repository;
 
 import com.example.cru.domain.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface PaymentRepository extends JpaRepository<Long, Payment> {
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    Optional<Payment>findByOrderId(Long orderId);
 }
