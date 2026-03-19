c4aedeb36eb3766aceea6caf6a0c96bf36bff250
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 19:33:35 +0900

    refactor: 사용 안하는 import 삭제

--- src/main/java/com/example/cru/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/cru/domain/payment/service/PaymentService.java
@@ -11,7 +11,6 @@ import com.example.cru.domain.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDateTime;
 
