324a697bafe8ad5cea5d8ee30977d1a9f1d3da07
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 16:49:23 +0900

    feat: 결제 승인 컨트롤러 구현

--- src/main/java/com/example/cru/domain/payment/controller/PaymentController.java
+++ src/main/java/com/example/cru/domain/payment/controller/PaymentController.java
@@ -1,11 +1,12 @@
 package com.example.cru.domain.payment.controller;
 
+import com.example.cru.common.model.CommonResponse;
+import com.example.cru.domain.payment.model.PaymentDto;
+import com.example.cru.domain.payment.model.request.SuccessPaymentRequest;
 import com.example.cru.domain.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -15,12 +16,14 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     //결제 승인
-    @PostMapping
-    public void successPay() {
+    @PostMapping("/order/{orderId}/payment")
+    public ResponseEntity<CommonResponse> successPay(@PathVariable Long orderId, @RequestBody SuccessPaymentRequest request) {
 
         //비지니스 로직
-        paymentService.successPayment();
+        PaymentDto response = paymentService.successPayment(orderId,request);
 
+        //Dto 반환
+        return ResponseEntity.ok(CommonResponse.success("결제가 완료 됐습니다.", response));
     }
 
     //결제 취소
