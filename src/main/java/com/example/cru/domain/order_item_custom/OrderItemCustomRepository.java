7af9bacfc4f8c7ee5cc38a3dc9579a36ef5d314e
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 17:24:27 +0900

    refactor: 패키지 이동 paymentRepository에서 order_item_custom 패키지로 이동

--- src/main/java/com/example/cru/domain/order_item_custom/OrderItemCustomRepository.java
+++ src/main/java/com/example/cru/domain/order_item_custom/OrderItemCustomRepository.java
@@ -1,4 +1,4 @@
-package com.example.cru.domain.payment.repository;
+package com.example.cru.domain.order_item_custom;
 
 import com.example.cru.domain.order_item_custom.entity.OrderItemCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
