5824d6eb86b058caed4c0a348c7f69ec9a85bddc
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 16:43:10 +0900

    feat: 결제 전 주문 조회에 사용될 정적 쿼리 추가

--- src/main/java/com/example/cru/domain/order/repository/OrderRepository.java
+++ src/main/java/com/example/cru/domain/order/repository/OrderRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Page<Order> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
+
+    // 결제 전 주문 조회
+    Optional<Order> findByOrderId(Long orderId);
 }
