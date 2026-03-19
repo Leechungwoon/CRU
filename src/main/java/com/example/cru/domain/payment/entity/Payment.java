cfcf0702d60ee30b6bb944881676805dcf52b178
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 16:59:38 +0900

    feat: - @Builder 추가, - @AllArgsConstructor, - isDeleted 기능 추가: 조회 안된 걸 삭제 된걸로 표시하는 용도
    -> @SQLRestriction: 부분에서 DB 삭제된 내용 조회 안되게 하는 기능
    
    isDeleted, @SQLRestriction 각자 다른 역할

--- src/main/java/com/example/cru/domain/payment/entity/Payment.java
+++ src/main/java/com/example/cru/domain/payment/entity/Payment.java
@@ -3,9 +3,7 @@ package com.example.cru.domain.payment.entity;
 import com.example.cru.common.enums.PaymentStatus;
 import com.example.cru.domain.order.entity.Order;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
@@ -13,7 +11,9 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "payments")
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @SQLRestriction("is_deleted = false")
 public class Payment {
 
@@ -38,4 +38,14 @@ public class Payment {
 
     @Column(name = "canceled_at")
     private LocalDateTime canceledAt; // 취소 시각
+
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    private boolean isDeleted = false; // 소프트 삭제
+
+    // 삭재 여부 표시
+    public void softDelete() {
+        this.isDeleted = true;
+    }
+    //결제 상태가 성공 및 취소만 있는 이유는 결제 MVP에서는 승인 실패 시 payment에 저장을 안하기 때문에 예외로 대체로 진행 추후 PG 연동 후 FAILED 상태 추가 예정
 }
