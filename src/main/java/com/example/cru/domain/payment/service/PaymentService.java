09e60e1b5b7d56c8655d4ec793e44038cf1b94b9
Leechungwoon <dlcjddns22@gmail.com>
2026-03-19 16:48:29 +0900

    feat: 결제 승인 로직 구현
    1. 주문조회
    2. 결제 전,후 여부 확인
    3. 결제 금액이 총 금액과 일치하는지 확인
    4. Payment 정보 저장 로직
    5. 결제 승인(success)시 paid로 상태 변경 로직

--- src/main/java/com/example/cru/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/cru/domain/payment/service/PaymentService.java
@@ -1,10 +1,18 @@
 package com.example.cru.domain.payment.service;
 
+import com.example.cru.common.enums.OrderStatus;
+import com.example.cru.common.enums.PaymentStatus;
+import com.example.cru.domain.order.entity.Order;
 import com.example.cru.domain.order.repository.OrderRepository;
+import com.example.cru.domain.payment.entity.Payment;
+import com.example.cru.domain.payment.model.PaymentDto;
+import com.example.cru.domain.payment.model.request.SuccessPaymentRequest;
 import com.example.cru.domain.payment.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -15,19 +23,45 @@ public class PaymentService {
 
     //결제 승인
     @Transactional
-    public void successPayment(){
+    public PaymentDto successPayment(Long orderId, SuccessPaymentRequest request) {
 
+        // 1. 주문 조회
+        Order order = orderRepository.findByOrderId(orderId)
+                .orElseThrow(() -> new RuntimeException("주문을 찾을 수 없습니다."));
+        //2. 결제 여부 확인
+        paymentRepository.findByOrderId(orderId).ifPresent(p -> {
+            throw new RuntimeException("이미 결제된 주문입니다.");
+        });
+
+        //  3.금액 검증
+        if (order.getTotalPrice() != request.getAmount()) {
+            throw new RuntimeException("결제 금액이 주문 금액과 다릅니다.");
+        }
+
+        // 4.Payment 저장
+        Payment payment = Payment.builder()
+                .order(order)
+                .amount(request.getAmount())
+                .status(PaymentStatus.SUCCESS)
+                .paidAt(LocalDateTime.now())
+                .build();
+        paymentRepository.save(payment);
+
+        // 5. Order 상태 변화 결제 승인시 되면 PENDING -> PAID 변경
+        order.updateStatus(OrderStatus.PAID);
+
+        return PaymentDto.from(payment);
     }
 
     //결제 취소
     @Transactional
-    public void canceledPayment(){
+    public void canceledPayment() {
 
     }
 
     //결제 조회
     @Transactional(readOnly = true)
-    public void getMyPayment(){
+    public void getMyPayment() {
 
     }
 
