e392a49a283e8379a480992b88771b02ed0fab1d
Leechungwoon <dlcjddns22@gmail.com>
2026-03-26 17:48:13 +0900

    feat: JWT 로그인 구현

---
+++ src/main/java/com/example/cru/domain/auth/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.example.cru.domain.auth.controller;
+
+import com.example.cru.common.model.CommonResponse;
+import com.example.cru.domain.auth.model.LoginRequest;
+import com.example.cru.domain.auth.model.LoginResponse;
+import com.example.cru.domain.auth.service.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<CommonResponse> loginApi(@RequestBody LoginRequest request) {
+        log.info("AuthController.loginApi()");
+
+        //비지니스 로직
+        LoginResponse response = authService.login(request);
+
+        //Dto 반환
+        return ResponseEntity.ok(CommonResponse.success("로그인에 성공했습니다.", response));
+    }
+}
