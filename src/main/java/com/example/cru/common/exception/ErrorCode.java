b9212e511829eb8ec0675f2dc04de0fd0748f903
Leechungwoon <dlcjddns22@gmail.com>
2026-04-10 17:35:24 +0900

    feat: S3 이미지 업로드 및 Presigned URL 구현
    - S3 에러 추가 및 정렬 맞춤

--- src/main/java/com/example/cru/common/exception/ErrorCode.java
+++ src/main/java/com/example/cru/common/exception/ErrorCode.java
@@ -9,16 +9,15 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     //ITEM
-    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND,"아이템을 찾을 수 없습니다."),
+    ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "아이템을 찾을 수 없습니다."),
 
     //USER
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"사용자를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
     //ORDER
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."), //404
     ORDER_NOT_PAYABLE(HttpStatus.BAD_REQUEST, "결제할 수 없는 주문입니다."), //400
     ORDER_NOT_CANCELABLE(HttpStatus.BAD_REQUEST, "취소할 수 없는 주문입니다."), //400
-
 
     //Payment
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "결제 정보를 찾을 수 없습니다."), //404
@@ -26,14 +25,18 @@ public enum ErrorCode {
     INVALID_AMOUNT(HttpStatus.BAD_REQUEST, "결제 금액이 주문 금액과 다릅니다."), //400
 
     //Auth
-    INVALID_PASSWORD(HttpStatus.INTERNAL_SERVER_ERROR,"비밀번호가 틀렸습니다." ),
+    INVALID_PASSWORD(HttpStatus.INTERNAL_SERVER_ERROR, "비밀번호가 틀렸습니다."),
+
+    //S3
+    INVALID_FILE_TYPE(HttpStatus.INTERNAL_SERVER_ERROR, "올바른 파일이 아닙니다."),
+    S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."),
 
     // 공통,
     UNAUTHORIZED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
     TOSS_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "결제 처리 중 오류가 발생했습니다."),
     INVALID_TOKEN(HttpStatus.INTERNAL_SERVER_ERROR, "발급된 토큰이 없습니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED,"토큰이 만료됐습니다.");
-
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료됐습니다."),
+    ;
 
     private final HttpStatus status; //HTTP 상태 코드
     private final String message; //클라이언트에게 반환할 메시지
