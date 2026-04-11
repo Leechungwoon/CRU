3d8b788eddb3916770054d65851a710cea111619
Leechungwoon <dlcjddns22@gmail.com>
2026-04-11 17:06:33 +0900

    feat: S3 이미지 업로드 및 Presigned URL 구현
    - S3Service: 이미지 삭제 구현

--- src/main/java/com/example/cru/domain/s3/S3Service.java
+++ src/main/java/com/example/cru/domain/s3/S3Service.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
@@ -31,7 +32,13 @@ public class S3Service {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
-    //파일 업로드 -> UUID 파일명 중복 방지
+    /**
+     * 이미지 업로드
+     *
+     * @param file   업로드할 이미지 파일
+     * @param folder 저장할 폴더
+     * @return 저장된 S3 키 (파일 경로)
+     */
     public String upload(MultipartFile file, String folder) {
 
         // 이미지 파일 검증
@@ -60,7 +67,14 @@ public class S3Service {
         return fileName; //저장된 키 반환 -> DB에 저장
     }
 
-    //Presigned URL 생성
+    /**
+     * Presigned URL 생성
+     * 버킷이 막혀있어도 임시로 이미지 접근 가능한 URL 발급
+     * 유효시간 10분 이후 자동 만료
+     *
+     * @param key S3 저장된 파일 경로
+     * @return
+     */
     public String presignedUrl(String key) {
 
         GetObjectRequest getObjectRequest = GetObjectRequest.builder()
@@ -75,5 +89,20 @@ public class S3Service {
         return s3Presigner.presignGetObject(presignRequest)
                 .url()
                 .toString();// 임시 URL 반환
+    }
+
+    /**
+     * 주문 취소 시 업로드된 이미지 삭제
+     *
+     * @param key S3에 저장된 파일 경로
+     */
+    public void delete(String key) {
+        DeleteObjectRequest request = DeleteObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        // S3에서 파일 삭제
+        s3Client.deleteObject(request);
     }
 }
